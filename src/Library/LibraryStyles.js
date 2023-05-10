2d549ab0eaf026bcf89e0a8b116ca9db5e1201b6
Harsh Wadhwa <80612437+harsh-wadhwa@users.noreply.github.com>
2023-05-10 16:38:25 +0530

    basic app with some libraries and basic frontend

---
+++ src/Library/LibraryStyles.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import library_image from "../assets/images/library_image.jpg"
+import dark_library from "../assets/images/dark_library.jpg"
+
+export const HomePageBackground = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background: url(${library_image});
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const HomePage = styled.div`
+    height: 95vh;
+    width: 95vw;
+    background: #ffffff;
+    border-radius: 15px;
+    position: relative;
+`
+
+export const Nav = styled.div`
+    width: 100%;
+    height: 10%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 32px;
+    font-weight: 800;
+`
+
+export const BooksContainer = styled.div`
+    width: 70%;
+    height: 80%;
+    position: absolute;
+    bottom: 10px;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    overflow: scroll;
+`
+
+export const FiltersContainer = styled.div`
+    width: 20%;
+    height: 90%;    
+    position: absolute;
+    bottom: 0;
+    right: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+`
+
+export const BookItemContainer = styled.div`
+    width: 50%;
+    height: 150px;
+    background: #d7e6fc;
+    border-radius: 10px;
+`
---
+++ src/Library/index.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { BookItemContainer, BooksContainer, FiltersContainer, HomePage, HomePageBackground, Nav } from './LibraryStyles';
+
+const books = [
+    { title: 'Book 1', author: 'Author 1', subject: 'Subject 1', publishDate: '2022-01-01' },
+    { title: 'Book 2', author: 'Author 2', subject: 'Subject 2', publishDate: '2022-02-01' },
+    { title: 'Book 3', author: 'Author 1', subject: 'Subject 3', publishDate: '2022-03-01' },
+    { title: 'Book 1', author: 'Author 1', subject: 'Subject 1', publishDate: '2022-01-01' },
+    { title: 'Book 2', author: 'Author 2', subject: 'Subject 2', publishDate: '2022-02-01' },
+    { title: 'Book 3', author: 'Author 1', subject: 'Subject 3', publishDate: '2022-03-01' },
+    { title: 'Book 1', author: 'Author 1', subject: 'Subject 1', publishDate: '2022-01-01' },
+    { title: 'Book 2', author: 'Author 2', subject: 'Subject 2', publishDate: '2022-02-01' },
+    { title: 'Book 3', author: 'Author 1', subject: 'Subject 3', publishDate: '2022-03-01' },
+    { title: 'Book 1', author: 'Author 1', subject: 'Subject 1', publishDate: '2022-01-01' },
+    { title: 'Book 2', author: 'Author 2', subject: 'Subject 2', publishDate: '2022-02-01' },
+    { title: 'Book 3', author: 'Author 1', subject: 'Subject 3', publishDate: '2022-03-01' },
+    // Add more books...
+];
+
+export const Homepage = () => {
+
+    const [titleFilter, setTitleFilter] = useState('');
+    const [authorFilter, setAuthorFilter] = useState('');
+    const [subjectFilter, setSubjectFilter] = useState('');
+    const [dateFilter, setDateFilter] = useState('');
+
+    const handleTitleFilterChange = (event) => {
+        setTitleFilter(event.target.value.toLowerCase());
+    };
+
+    const handleAuthorFilterChange = (event) => {
+        setAuthorFilter(event.target.value.toLowerCase());
+    };
+
+    const handleSubjectFilterChange = (event) => {
+        setSubjectFilter(event.target.value.toLowerCase());
+    };
+
+    const handleDateFilterChange = (event) => {
+        setDateFilter(event.target.value.toLowerCase());
+    };
+
+    const filteredBooks = books.filter((book) =>
+        book.title.toLowerCase().includes(titleFilter) &&
+        book.author.toLowerCase().includes(authorFilter) &&
+        book.subject.toLowerCase().includes(subjectFilter) &&
+        book.publishDate.toLowerCase().includes(dateFilter)
+    );
+
+    return (
+        <HomePageBackground>
+
+            <HomePage>
+
+                <Nav>
+                    The Library
+                </Nav>
+
+                <BooksContainer>
+                    {filteredBooks.map((book, index) => (
+                        <BookItemContainer key={index}>
+                            <p>{book.title}</p>
+                            <p>Author: {book.author}</p>
+                            <p>Subject: {book.subject}</p>
+                            <p>Publish Date: {book.publishDate}</p>
+                        </BookItemContainer>
+                    ))}
+                </BooksContainer>
+
+
+                <FiltersContainer>
+                    <h2>Filters</h2>
+                    <input
+                        type="text"
+                        placeholder="Filter by Title"
+                        value={titleFilter}
+                        onChange={handleTitleFilterChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Filter by Author"
+                        value={authorFilter}
+                        onChange={handleAuthorFilterChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Filter by Subject"
+                        value={subjectFilter}
+                        onChange={handleSubjectFilterChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Filter by Publish Date"
+                        value={dateFilter}
+                        onChange={handleDateFilterChange}
+                    />
+                </FiltersContainer>
+
+
+            </HomePage>
+
+
+        </HomePageBackground>
+    )
+}
